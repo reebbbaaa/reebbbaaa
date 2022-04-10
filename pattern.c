b1924c078a23232cc9ea0106b1ec1dd832d9cb46
reebbbaaa <103366123+reebbbaaa@users.noreply.github.com>
2022-04-10 18:30:40 +0545

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,j;
+	for(i=0;i<6;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+		printf("%c",(char)(i+64));
+	}
+	printf("\n");
+}
+	return 0;
+	}
+
